f603d04a0710d4dcdbd508af5923cf8cb1b1fcbd
wongchichong <wongchichong@gmail.com>
2024-02-06 10:44:11 +0800

    bump version

--- docs/index.tsx
+++ docs/index.tsx
@@ -38,7 +38,7 @@ const App = () => <div class='w-[25%]'>
                         <svg class="select-none w-[1em] h-[1em] inline-block fill-current shrink-0 text-2xl [transition:fill_200ms_cubic-bezier(0.4,0,0.2,1)0ms]" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DraftsIcon"><path d="M21.99 8c0-.72-.37-1.35-.94-1.7L12 1 2.95 6.3C2.38 6.65 2 7.28 2 8v10c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2l-.01-10zM12 13 3.74 7.84 12 3l8.26 4.84L12 13z"></path></svg>
                     </ListItemIcon>
                     <ListItemText primary="Drafts" secondary="Jan 7, 2014" />
-                    {/* <Collapse in={open} timeout="auto" unmountOnExit>
+                    {/* <Collapse open={open} timeout="auto" unmountOnExit>
                         <List component="div" disablePadding>
                             <ListItemButton sx={{ pl: 4 }}>
                                 <ListItemIcon>
--- src/lib/MenuList.tsx
+++ src/lib/MenuList.tsx
@@ -1,6 +1,4 @@
 import { tw } from 'woby-styled'
 
-import type { JSX } from 'woby/jsx-runtime'
-
 export const MenuList = tw('ul')`relative m-0 px-0 py-2 list-none`
 export const List = MenuList
