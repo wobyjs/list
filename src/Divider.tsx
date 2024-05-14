298df124a3f9924fc18e9fe2ce745195e12150f2
wongchichong <wongchichong@gmail.com>
2024-05-14 12:13:48 +0800

    Bump version

--- src/Divider.tsx
+++ src/Divider.tsx
@@ -7,6 +7,6 @@ export const Divider = ({ vertical, className, ...props }: JSX.HrHTMLAttributes<
 
     //<hr class="shrink-0 h-auto self-stretch mx-1 m-0 border-[0px_thin_0px_0px] border-solid border-[rgba(0,0,0,0.12)]" >
 
-    return <hr class={[() => $$(vertical) ? 'shrink-0 h-auto self-stretch mx-1 m-0 [border:width:0px_thin_0px_0px] border-solid [border-color:rgba(0,0,0,0.12)]' : 'shrink-0 my-2 m-0 [border-width:0px_0px_thin] border-solid', cls ?? className]} />
+    return <hr class={[() => $$(vertical) ? 'bg-[rgb(223,221,221)] inline-block h-4 w-0.5 mt-[0em] -mb-1 mx-[1em]' : 'shrink-0 my-2 m-0 [border-width:0px_0px_thin] border-solid', cls ?? className]} />
 }
 
